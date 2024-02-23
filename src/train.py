afde4fd4b3e11490c9b52d767d4de2db1ee7b1b8
abdessalam-eddib <abdesedd@outlook.fr>
2024-02-23 11:49:23 +0100

    Q-fit-test with ExtraTrees

--- src/train.py
+++ src/train.py
@@ -1,8 +1,18 @@
 from gymnasium.wrappers import TimeLimit
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from env_hiv import HIVPatient
 
+import time
+import torch
+import torch.nn as nn
+from torch.optim import AdamW
+import torch.nn.functional as F
+import pickle as pkl
+import numpy as np
+from tqdm import tqdm
+
 env = TimeLimit(
-    env=HIVPatient(domain_randomization=False), max_episode_steps=200
+    env=HIVPatient(domain_randomization=False, logscale=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
 # Now is the floor is yours to implement the agent and train it.
 
@@ -10,12 +20,97 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+
 class ProjectAgent:
+
+    def __init__(self):
+        self.path = "model"
+        self.gamma = 0.98
+        self.Q = ExtraTreesRegressor()
+
+        
+
     def act(self, observation, use_random=False):
-        return 0
+        Qsa = []
+        for a in range(self.n_action):
+            sa = np.append(observation,a).reshape(1, -1)
+            Qsa.append(self.Q.predict(sa))
+        return np.argmax(Qsa)
+
+    def collect_samples(self, env, horizon, exp = 0):
+        s, _ = env.reset()
+        S = []
+        A = []
+        R = []
+        S2 = []
+        D = []
+        for _ in tqdm(range(horizon)):
+            if exp == 0:
+                a = env.action_space.sample()
+            else:
+                p = np.random.rand()
+                if p < 0.15:
+                    a = env.action_space.sample()
+                else:
+                    a = self.act(s)
+
+            s2, r, done, trunc, _ = env.step(a)
+            S.append(s)
+            A.append(a)
+            R.append(r)
+            S2.append(s2)
+            D.append(done)
+            if done or trunc:
+                s, _ = env.reset()
+            else:
+                s = s2
+        S = np.array(S)
+        A = np.array(A).reshape((-1,1))
+        R = np.array(R)
+        S2= np.array(S2)
+        D = np.array(D)
+        return S, A, R, S2, D
+    def train(self, env, n_iterations, n_exp, horizon=30 * 200):
+        self.state_dim = env.observation_space.shape[0]
+        self.n_action = env.action_space.n
+        for exp in range(n_exp):
+            S, A, R, S2, D = self.collect_samples(env, exp=exp, horizon=horizon)
+            nb_samples = S.shape[0]
+            SA = np.append(S,A,axis=1)
+            for iter in tqdm(range(n_iterations)):
+                if iter==0:
+                    value=R.copy()
+                else:
+                    Q2 = np.zeros((nb_samples,self.n_action))
+                    for a2 in range(self.n_action):
+                        A2 = a2*np.ones((S.shape[0],1))
+                        S2A2 = np.append(S2,A2,axis=1)
+                        Q2[:,a2] = self.Q.predict(S2A2)
+                    max_Q2 = np.max(Q2,axis=1)
+                    value = R + self.gamma*(1-D)*max_Q2
+
+                Q = ExtraTreesRegressor()
+                Q.fit(SA,value)
+                self.Q = Q
+    
+
 
     def save(self, path):
-        pass
+        payload = {}
+        payload["Q"] = self.Q
+        payload["nb_action"] = self.n_action
+        with open(self.path, "wb") as f:
+            pkl.dump(payload, f)
+        
 
     def load(self):
-        pass
+        with open(self.path, "rb") as f:
+            payload = pkl.load(f)
+            self.Q = payload["Q"]
+            self.n_action = payload["nb_action"]
+
+
+
+agent = ProjectAgent()
+agent.train(env, n_iterations=400, n_exp=13, horizon=6000)
+agent.save("model")
